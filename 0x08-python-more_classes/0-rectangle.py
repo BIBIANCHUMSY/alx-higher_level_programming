2f657ba2c6126ff95c52386b850b9bbfe2e3c90b
BIBIANCHUMSY <bbchumsy@gmail.com>
2023-04-04 01:18:41 -0700

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+a module that defines a rectangle
+class Rectangle:
+    This class have no attribute
+    pass 
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This repository is about 0x08 Python-more_classes
